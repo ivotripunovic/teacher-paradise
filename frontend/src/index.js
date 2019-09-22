b389b1fa17921057e4603becc2c5fcd3251d44df
Ivo Tripunovic <tripunovic@yahoo.com>
2019-09-22 14:18:47 +0200

    Initial code for frontend application

---
+++ frontend/src/index.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+// import axios from 'axios';
+
+const studentsStub = {
+  0: {'name': 'Ivo', 'dates': ['2019-12-03']},
+  1: {'name': 'Tana', 'dates': []},
+  2: {'name': 'Natalija', 'dates': []}
+}
+
+// const URL = "http://127.0.0.1:5000";
+
+const Main = () => {
+  const [students, setStudents] = useState(null);
+
+  const fetchStudents = async () => {
+    // const response = await axios.get(URL + "/students");
+    // const responseOk = response && response.status === 200;
+    // if (responseOk) {
+    //   setStudents(await response.data);
+    // }
+    setStudents(studentsStub);
+  };
+
+  useEffect(() => {
+    fetchStudents();
+  }, []);
+
+  return (
+    <div>
+      <Students students={students} />
+      <Dates />
+      <Statistic />
+    </div>
+  );
+};
+
+const Students = ({ students }) => {
+  return (
+    <div>
+      [Students]
+      <ul>
+        {students && Object.values(students).map((x, id) => <li key={id}>{x.name}</li>)}
+        {!students && <span>Please add student</span>}
+      </ul>
+    </div>
+  );
+};
+
+const Dates = () => {
+  return <div>Dates...</div>;
+};
+
+const Statistic = () => {
+  return <div>Statistic</div>;
+};
+
+ReactDOM.render(<Main />, document.getElementById("root"));
