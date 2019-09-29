b7d5fe5da1bef62f7ee27d2082211068c98d4e72
Ivo Tripunovic <tripunovic@yahoo.com>
2019-09-29 09:31:42 +0200

    Saving to sql instead of in memory list

--- backend/app.py
+++ backend/app.py
@@ -2,47 +2,44 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
+import datetime
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./app.db' #'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 CORS(app)
 
 url_students = '/students'
 
-students = {
-    0: {'name': 'Ivo', 'dates': ['2019-12-03']},
-    1: {'name': 'Tana', 'dates': []},
-    2: {'name': 'Natalija', 'dates': []}
-}
-
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(12), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
-   
+
+
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), 
-            nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+                        nullable=False)
     name = db.Column(db.String(80), nullable=False)
 
     def __repr__(self):
         return '<Student %r>' % self.name
 
     def to_json(self):
-        return { 'id': self.id, 'name': self.name }
+        return {'id': self.id, 'name': self.name}
+
 
 class StudentClass(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    student_id = db.Column(db.Integer, db.ForeignKey('student.id'), 
-            nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'),
+                           nullable=False)
     class_date = db.Column(db.DateTime, nullable=False)
-
 
 
 @app.route(url_students, methods=['get'])
@@ -53,32 +50,53 @@ def get():
 
 @app.route(url_students, methods=['post'])
 def create_student():
-    id = len(students)
-    students[id] = request.get_json()
-    return students[id]
+    user_id = 1  # TODO set logged user id
+    data = request.get_json() or {}
+
+    student = Student(user_id=user_id, name=data['name'])
+    db.session.add(student)
+    db.session.commit()
+    return student.to_json()
 
 
 @app.route(url_students + '/<int:id>', methods=['get'])
 def get_student(id):
-    return students[id]
+    # todo check logged in user
+    s = Student.query.get_or_404(id)
+    res = s.to_json()
+    dates = StudentClass.query.filter_by(student_id=id).all()
+    res['dates'] = [x.class_date.strftime('%Y-%m-%d') for x in dates]
+
+    return res
 
 
 @app.route(url_students + '/<int:id>', methods=['put'])
 def update_student(id):
-    students[id]['name'] = request.json['name']
-    return students[id]
+    s = Student.query.get_or_404(id)
+    data = request.get_json() or {}
+    s.name = data['name']
+    db.session.commit()
+    return s.to_json()
 
 
 @app.route(url_students + '/<int:id>/dates/<string:date>', methods=['post'])
 def add_date_student(id, date):
-    students[id]['dates'].append(date)
-    return students[id]
+    # TODO check logged in user
+    d = datetime.datetime.strptime(date, '%Y-%m-%d')
+    s = StudentClass(student_id=id, class_date=d)
+    db.session.add(s)
+    db.session.commit()
+    return ""
 
 
 @app.route(url_students + '/<int:id>/dates/<string:date>', methods=['delete'])
 def delete_date_student(id, date):
-    students[id]['dates'].remove(date)
-    return students[id]
+    # TODO check logged in user
+    d = datetime.datetime.strptime(date, '%Y-%m-%d')
+    s = StudentClass.query.filter_by(student_id=id, class_date=d).first()
+    db.session.delete(s)
+    db.session.commit()
+    return "", 204
 
 
 if __name__ == '__main__':
--- backend/test_app.py
+++ backend/test_app.py
@@ -8,14 +8,42 @@ add date
 remove date
 """
 import unittest
+import os
+import datetime
 
 from flask import json
-from app import app
+from app import app, db, User, Student, StudentClass
 
 url = '/students'
+TESTDB = 'test.db'
 
 
 class StudentTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./' + TESTDB
+        app.config['TESTING'] = True
+        db.create_all()
+
+        u = User(username='Admin', email='admin@net.com')
+        sess = db.session
+        sess.add(u)
+        sess.commit()
+
+        for n in ['Ivo', 'Tana', 'Natalija']:
+            s = Student(name=n, user_id=u.id)
+            sess.add(s)
+        sess.commit()
+        s = Student.query.get(1)
+        date = datetime.datetime.strptime('2016-08-29', '%Y-%m-%d')
+        sess.add(StudentClass(student_id=s.id, class_date=date))
+        sess.commit()
+
+    @classmethod
+    def tearDownClass(cls):
+        db.drop_all()
+        os.remove(TESTDB)
 
     def test_create_student(self):
         tester = app.test_client(self)
@@ -32,7 +60,7 @@ class StudentTest(unittest.TestCase):
 
     def test_get_user(self):
         tester = app.test_client(self)
-        resp = tester.get(url + '/1')
+        resp = tester.get(url + '/2')
         self.assertEqual(200, resp.status_code)
         self.assertEqual('Tana', resp.get_json()['name'])
 
@@ -50,19 +78,19 @@ class StudentTest(unittest.TestCase):
 
     def test_add_date(self):
         tester = app.test_client(self)
-        resp = tester.post(url + '/1/dates/2016-08-29')
+        resp = tester.post(url + '/2/dates/2016-08-29')
         self.assertEqual(200, resp.status_code)
 
-        resp = tester.get(url + '/1')
+        resp = tester.get(url + '/2')
         self.assertEqual(200, resp.status_code)
         self.assertEqual('2016-08-29', resp.get_json()['dates'][0])
 
     def test_delete_date(self):
         tester = app.test_client(self)
-        resp = tester.delete(url + '/1/dates/2016-08-29')
-        self.assertEqual(200, resp.status_code)
+        resp = tester.delete(url + '/2/dates/2016-08-29')
+        self.assertEqual(204, resp.status_code)
 
-        resp = tester.get(url + '/1')
+        resp = tester.get(url + '/2')
         self.assertEqual(200, resp.status_code)
         self.assertEqual(0, len(resp.get_json()['dates']))
 
