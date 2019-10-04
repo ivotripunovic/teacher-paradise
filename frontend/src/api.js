1c3beeaeda2bc1fbc0b4b705d6fdbb37be103c64
Ivo Tripunovic <tripunovic@yahoo.com>
2019-10-05 00:05:58 +0200

    Adding api calls, with e2e tests

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -7,3 +7,14 @@ const STUDENT_PATH = "/students";
 export const getStudents = () => axios.get(STUDENT_PATH);
 
 export const createStudent = name => axios.post(STUDENT_PATH, { name });
+
+export const getStudent = id => axios.get(STUDENT_PATH + `/${id}`);
+
+export const updateStudent = ({ id, name }) =>
+  axios.put(STUDENT_PATH + `/${id}`, { id, name });
+
+export const addClassDate = ({ studentId, date }) =>
+  axios.post(STUDENT_PATH + `/${studentId}/dates/${date}`);
+
+export const deleteClassDate = ({ studentId, date }) =>
+  axios.delete(STUDENT_PATH + `/${studentId}/dates/${date}`);
--- frontend/src/api.test.js
+++ frontend/src/api.test.js
@@ -1,12 +1,42 @@
-import { getStudents, createStudent } from "./api";
+import {
+  getStudents,
+  createStudent,
+  getStudent,
+  updateStudent,
+  addClassDate,
+  deleteClassDate
+} from "./api";
 
-test("Api getStudents", async () => {
-  const result = await getStudents();
-  expect(result.status).toBe(200);
-});
+describe("API calls", () => {
+  test("getStudents", async () => {
+    const result = await getStudents();
+    expect(result.status).toBe(200);
+  });
 
-test("Api createStudent", async () => {
-  const result = await createStudent("Ivo");
-  expect(result.status).toBe(200);
-  expect(result.data.name).toBe("Ivo");
+  test("createStudent", async () => {
+    const result = await createStudent("Ivo");
+    expect(result.status).toBe(200);
+    expect(result.data.name).toBe("Ivo");
+  });
+
+  test("getStudent by id", async () => {
+    const result = await getStudent(1);
+    expect(result.data.name).toBeTruthy();
+    expect(result.data.dates).toBeTruthy();
+  });
+
+  test("Update student", async () => {
+    const result = await updateStudent({ id: 1, name: "Tana" });
+    expect(result.data.name).toBe("Tana");
+  });
+
+  test("Add class date", async () => {
+    const result = await addClassDate({ studentId: 1, date: "2010-12-25" });
+    expect(result.status).toBe(200);
+  });
+
+  test("Delete class date", async () => {
+    const result = await deleteClassDate({ studentId: 1, date: "2010-12-25" });
+    expect(result.status).toBe(204);
+  });
 });
