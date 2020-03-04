d622b302ba4d363ddb996b4ac032dcd3c857608f
Ivo Tripunovic <tripunovic@yahoo.com>
2020-03-04 22:38:13 +0100

    Moved statistic in separete modul, and made it sort descending by date

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import "./index.css";
 import parse from "date-fns/parse";
 import { FixedSizeList as List } from "react-window";
 import { Login } from "./components/login";
+import { Statistic } from "./components/statistic";
 
 const loginSubmit = (setLogin, setError) => e => {
   e.preventDefault();
@@ -120,47 +121,6 @@ const Students = ({
         <input type="text" name="name" />
         <input type="submit" value="Submit" />
       </form>
-    </div>
-  );
-};
-
-const countAttendings = student => {
-  if (!student) {
-    return [];
-  }
-  const dates = student.dates.map(d => new Date(d));
-  const result = new Map();
-
-  for (let d of dates) {
-    const key = d.getFullYear() * 100 + d.getMonth();
-    let value = result.get(key);
-    if (value === undefined) {
-      value = 0;
-    }
-    value += 1;
-    result.set(key, value);
-  }
-  return Array.from(result);
-};
-
-const Statistic = ({ student }) => {
-  const stats = countAttendings(student);
-  const toMonth = yearMonth => {
-    const m = yearMonth % 100;
-    const y = Math.floor(yearMonth / 100);
-    const month = new Date(y, m, 1).toLocaleString('default', { month: 'long' });
-    return y + '. ' + month;
-  }
-  return (
-    <div className="box">
-      <h3>Statistic</h3>
-      <ul>
-      {stats.map(e => (
-        <li key={e[0]}>
-          {toMonth(e[0])} - {e[1]}
-        </li>
-      ))}
-      </ul>
     </div>
   );
 };
